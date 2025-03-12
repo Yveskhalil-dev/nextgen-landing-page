4793d62389618acd02464f2cf50ef6c9a38c4af6
yveskhalil-dev <yveskhalil9@gmail.com>
2025-03-12 22:00:13 +0200

    nextgen landing page template

---
+++ src/sections/ProductShowcase.tsx
@@ -0,0 +1,69 @@
+"use client";
+import successfulSoftwareSolutionsImage from "@/assets/successful-software-solutions.png";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
+export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  return (
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Innovate with NextGen</div>
+          </div>
+
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            Transform Your Business with Cutting-Edge Solutions
+          </h2>
+          <p className="section-des mt-5">
+            At NextGen, we provide state-of-the-art software solutions that
+            empower your business to reach new heights. Experience seamless
+            integration, enhanced productivity, and unparalleled support.
+          </p>
+        </div>
+
+        <div className="relative">
+          <Image
+            src={successfulSoftwareSolutionsImage}
+            alt="Successful Software Solutions"
+            className="mt-10"
+          />
+          <motion.img
+            src={pyramidImage.src}
+            alt="Pyramid image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY: translateY,
+            }}
+          />
+          <motion.img
+            src={tubeImage.src}
+            alt="Tube image"
+            height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY: translateY,
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
